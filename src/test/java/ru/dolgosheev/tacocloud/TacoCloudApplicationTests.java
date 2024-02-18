26b12f52fceae70fbb32a215ab72bdb367688a06
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-02-19 00:07:17 +0300

    initial commit

---
+++ src/test/java/ru/dolgosheev/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dolgosheev.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
