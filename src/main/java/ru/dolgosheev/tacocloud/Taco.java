1ab95e5b77d2773a2f12d2a24eca03cea5369928
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-02-20 16:29:43 +0300

    update 2024.02.20

---
+++ src/main/java/ru/dolgosheev/tacocloud/Taco.java
@@ -0,0 +1,13 @@
+package ru.dolgosheev.tacocloud;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+
+}
