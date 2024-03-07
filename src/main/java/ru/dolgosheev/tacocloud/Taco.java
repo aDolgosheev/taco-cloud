4100aee46c3cf2d6890f3d0e6987b5b11d42edd2
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-03-07 15:46:39 +0300

    update 2024.03.07

--- src/main/java/ru/dolgosheev/tacocloud/Taco.java
+++ src/main/java/ru/dolgosheev/tacocloud/Taco.java
@@ -4,10 +4,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private long id;
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
@@ -16,4 +19,6 @@ public class Taco {
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    private Date createdAt = new Date();
 }
